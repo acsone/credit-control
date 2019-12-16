e3f86561ff968196a2105b0686c1a0a7afc57b8b
Quentin Groulard <quentin.groulard@acsone.eu>
2019-12-16 17:33:18 +0100

    [REF] Remove update of auto_process on lines when changed on policy
    Extra complexity not much needed.
    A change on a policy usually doesn't impact credit lines previously created for that policy regarding the module's general behavior.

--- account_credit_control/models/credit_control_policy.py
+++ account_credit_control/models/credit_control_policy.py
@@ -48,43 +48,6 @@ class CreditControlPolicy(models.Model):
         "to have all credit control lines on same communication level",
     )
 
-    def write(self, values):
-        res = super().write(values)
-        if "auto_process_lower_levels" in values:
-            credit_control_model = self.env["credit.control.line"]
-            if values["auto_process_lower_levels"]:
-                res = credit_control_model.read_group(
-                    domain=[
-                        ("policy_id", "=", self.id),
-                        ("state", "in", ("draft", "to_be_sent")),
-                    ],
-                    fields=["id"],
-                    groupby=["partner_id", "currency_id"],
-                    lazy=True,
-                )
-                for group in res:
-                    highest_level_line = credit_control_model.search(
-                        group["__domain"],
-                        order="level DESC, date_due ASC",
-                        limit=1,
-                    )
-                    if not highest_level_line:
-                        continue
-                    highest_level_line.write({"auto_process": "highest_level"})
-                    highest_level_line.get_related_lines(
-                        exclude_ids=highest_level_line.ids
-                    ).write({"auto_process": "low_level"})
-            else:
-                lines = credit_control_model.search(
-                    [
-                        ("policy_id", "=", self.id),
-                        ("state", "in", ("draft", "to_be_sent")),
-                    ]
-                )
-                if lines:
-                    lines.write({"auto_process": "no_auto_process"})
-        return res
-
     def _move_lines_domain(self, credit_control_run):
         """Build the default domain for searching move lines"""
         self.ensure_one()
