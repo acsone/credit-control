bd0aaf00a38e072140ea813b7c529026afea701e
Quentin Groulard <quentin.groulard@acsone.eu>
2020-01-17 16:49:24 +0100

    [IMP] Set some methods private

--- account_credit_control/models/credit_control_communication.py
+++ account_credit_control/models/credit_control_communication.py
@@ -153,7 +153,7 @@ class CreditControlCommunication(models.Model):
                 )
                 group_lines = self.env["credit.control.line"].browse()
             if line not in group_lines:
-                group_lines |= line.get_lower_related_lines() or line
+                group_lines |= line._get_lower_related_lines() or line
             prev_group = group
             prev_policy_level = policy_level
         yield (
--- account_credit_control/wizard/credit_control_marker.py
+++ account_credit_control/wizard/credit_control_marker.py
@@ -46,7 +46,7 @@ class CreditControlMarker(models.TransientModel):
     def _filter_lines(self, lines):
         """get line to be marked filter done lines"""
         line_obj = self.env["credit.control.line"]
-        lines_and_related = lines.mapped(lambda l: l.get_lower_related_lines())
+        lines_and_related = lines.mapped(lambda l: l._get_lower_related_lines())
         domain = [("state", "!=", "sent"), ("id", "in", lines_and_related.ids)]
         return line_obj.search(domain)
 
